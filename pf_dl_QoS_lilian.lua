17aeea4b75b1c50278cc873471d9ff701ad5c50f
liliancf <liliancoelhofr@gmail.com>
2026-03-31 23:48:12 -0500

    Update pf_dl_QoS_lilian.lua

--- pf_dl_QoS_lilian.lua
+++ pf_dl_QoS_lilian.lua
@@ -158,9 +158,9 @@ function compute_dl_allocations(metrics_ptr, n_ues, total_rbs, min_rbs, rb_mask_
         -- 69 -> URLLC
         -- otherwise -> eMBB
         if m.uid == 0 then
-            return "URLLC"
-        else
             return "eMBB"
+        else
+            return "URLLC"
         end
     end
 
